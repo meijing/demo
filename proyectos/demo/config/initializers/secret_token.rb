8d6c8277c84d28d36d2826fd8defba6061abc4f5
paula <paula@paula-VirtualBox.(none)>
2011-07-28 17:20:19 +0200

    subida

---
+++ proyectos/demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '731f903b909b85ada90a35f3e54aadc911803014b05ba291d6dad79b9cfb7ffa26d5885e9351a336b8febf665209b269e43322654ad87c39744ec9f72c4683ea'
